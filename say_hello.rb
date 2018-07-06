92c7a5036221a311386e0b7078d38a2717cd9f3a
Cassandra Montoya <montoya.c@outlook.com>
2018-07-06 03:04:19 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,9 +1,4 @@
-def say_hello (name = "Kent Beck")
-  puts "Hello #{name}!"
+def say_hello (name = "Ruby Programmer")
+  puts "Hello ("Kent Beck")!"
 end
-  # Build your say_hello method here
-  
-  def say_hello (name= "Ruby Programmer")
-    puts "Hello #{"name"}!"
-  end
-  
+ 
